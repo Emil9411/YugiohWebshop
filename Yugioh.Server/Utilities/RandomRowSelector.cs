e7c5bc5763316c2fd188ad01d62c42c9710d46c8
Emil9411 <almadi.emil94@gmail.com>
2024-05-21 22:05:26 +0200

    Class to handle random row selection from the database created, interface implemented.

---
+++ Yugioh.Server/Utilities/RandomRowSelector.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Yugioh.Server.Context;
+using Yugioh.Server.Model;
+
+namespace Yugioh.Server.Utilities
+{
+    public class RandomRowSelector : IRandomRowSelector
+    {
+        private readonly Random _random;
+        private readonly CardsContext _context;
+
+        public RandomRowSelector(CardsContext context)
+        {
+            _random = new Random();
+            _context = context;
+        }
+
+        public async Task<object> GetRandomCardAsync()
+        {
+            var monsterCardsQuery = from monster in _context.MonsterCards
+                                    select new CardDto
+                                    {
+                                        CardId = monster.CardId,
+                                        Name = monster.Name,
+                                        Type = "Monster"
+                                    };
+
+            var spellAndTrapCardsQuery = from spellAndTrap in _context.SpellAndTrapCards
+                                         select new CardDto
+                                         {
+                                             CardId = spellAndTrap.CardId,
+                                             Name = spellAndTrap.Name,
+                                             Type = "Spell/Trap"
+                                         };
+
+            var allCardsQuery = monsterCardsQuery.Concat(spellAndTrapCardsQuery);
+
+            var randomCard = await allCardsQuery.OrderBy(x => Guid.NewGuid()).FirstOrDefaultAsync();
+
+            return randomCard;
+        }
+
+    }
+}
