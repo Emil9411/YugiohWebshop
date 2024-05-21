47fbfc4934a973e448f5cf8c7140c2cbe7aecead
Emil9411 <almadi.emil94@gmail.com>
2024-05-21 21:56:09 +0200

    Necessary migrations created, and applied.

---
+++ Yugioh.Server/Migrations/20240517160008_InitialCreate.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Yugioh.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MonsterCards",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Attack = table.Column<int>(type: "int", nullable: false),
+                    Defense = table.Column<int>(type: "int", nullable: true),
+                    Level = table.Column<int>(type: "int", nullable: true),
+                    Attribute = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Scale = table.Column<int>(type: "int", nullable: true),
+                    LinkValue = table.Column<int>(type: "int", nullable: true),
+                    LinkMarkers = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FrameType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Race = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Archetype = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    YgoProDeckUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MonsterCards", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SpellAndTrapCards",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FrameType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Race = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Archetype = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    YgoProDeckUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SpellAndTrapCards", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MonsterCards_Name",
+                table: "MonsterCards",
+                column: "Name",
+                unique: true,
+                filter: "[Name] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SpellAndTrapCards_Name",
+                table: "SpellAndTrapCards",
+                column: "Name",
+                unique: true,
+                filter: "[Name] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MonsterCards");
+
+            migrationBuilder.DropTable(
+                name: "SpellAndTrapCards");
+        }
+    }
+}
---
+++ Yugioh.Server/Migrations/20240517231638_ModifiedBaseClass.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Yugioh.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifiedBaseClass : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_SpellAndTrapCards_Name",
+                table: "SpellAndTrapCards");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MonsterCards_Name",
+                table: "MonsterCards");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CardId",
+                table: "SpellAndTrapCards",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CardId",
+                table: "MonsterCards",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SpellAndTrapCards_Name_CardId",
+                table: "SpellAndTrapCards",
+                columns: new[] { "Name", "CardId" },
+                unique: true,
+                filter: "[Name] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MonsterCards_Name_CardId",
+                table: "MonsterCards",
+                columns: new[] { "Name", "CardId" },
+                unique: true,
+                filter: "[Name] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_SpellAndTrapCards_Name_CardId",
+                table: "SpellAndTrapCards");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MonsterCards_Name_CardId",
+                table: "MonsterCards");
+
+            migrationBuilder.DropColumn(
+                name: "CardId",
+                table: "SpellAndTrapCards");
+
+            migrationBuilder.DropColumn(
+                name: "CardId",
+                table: "MonsterCards");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SpellAndTrapCards_Name",
+                table: "SpellAndTrapCards",
+                column: "Name",
+                unique: true,
+                filter: "[Name] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MonsterCards_Name",
+                table: "MonsterCards",
+                column: "Name",
+                unique: true,
+                filter: "[Name] IS NOT NULL");
+        }
+    }
+}
