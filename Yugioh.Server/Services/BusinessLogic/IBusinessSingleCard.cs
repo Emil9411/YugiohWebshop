76f63ab7cfd4d87c088eb8097d880830b792b53c
Emil9411 <almadi.emil94@gmail.com>
2024-05-21 22:03:31 +0200

    Interface for handling single-card business logic created.

---
+++ Yugioh.Server/Services/BusinessLogic/IBusinessSingleCard.cs
@@ -0,0 +1,10 @@
+﻿using Yugioh.Server.Model;
+
+namespace Yugioh.Server.Services.BusinessLogic
+{
+    public interface IBusinessSingleCard
+    {
+        Task<Card?> GetCardByNameAsync(string name);
+        Task<Card?> GetCardByCardIdAsync(int cardId);
+    }
+}
