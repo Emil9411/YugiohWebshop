63a206246a8746e4814d5e3232656bec4948d311
Emil9411 <almadi.emil94@gmail.com>
2024-05-21 21:53:38 +0200

    Abstract card model class created.

---
+++ Yugioh.Server/Model/Card.cs
@@ -0,0 +1,17 @@
+﻿namespace Yugioh.Server.Model
+{
+    public abstract class Card
+    {
+        public int Id { get; set; }
+        public int CardId { get; set; }
+        public string? Name { get; set; }
+        public string? Type { get; set; }
+        public string? FrameType { get; set; }
+        public string? Race { get; set; }
+        public string? Archetype { get; set; }
+        public string? Description { get; set; }
+        public string? YgoProDeckUrl { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? Price { get; set; }
+    }
+}
