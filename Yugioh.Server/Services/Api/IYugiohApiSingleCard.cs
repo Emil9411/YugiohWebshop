6e9a9f876b9708c3cf50d15d5ca225d2dcff0764
Emil9411 <almadi.emil94@gmail.com>
2024-05-21 21:58:44 +0200

    Interface for handling single-card API calls created.

---
+++ Yugioh.Server/Services/Api/IYugiohApiSingleCard.cs
@@ -0,0 +1,8 @@
+﻿namespace Yugioh.Server.Services.Api
+{
+    public interface IYugiohApiSingleCard
+    {
+        Task<string?> YugiohCardByNameAsync(string name);
+        Task<string?> YugiohCardByCardIdAsync(int cardId);
+    }
+}
