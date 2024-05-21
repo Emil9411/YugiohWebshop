b7117d70b522ef5a6b7723e429a121d0d34f6b3a
Emil9411 <almadi.emil94@gmail.com>
2024-05-21 21:57:22 +0200

    Model to handle some base data created.

---
+++ Yugioh.Server/Model/CardDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Yugioh.Server.Model
+{
+    public class CardDto
+    {
+        public int CardId { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; } // Indicate the type of card (Monster, Spell/Trap, etc.)
+    }
+
+}
