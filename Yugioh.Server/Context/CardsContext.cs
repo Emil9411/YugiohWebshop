19d1684de74cea6d05df9461c3963da86c48d192
Emil9411 <almadi.emil94@gmail.com>
2024-05-21 21:55:33 +0200

    Context for handling cards added.

---
+++ Yugioh.Server/Context/CardsContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Yugioh.Server.Model;
+
+namespace Yugioh.Server.Context
+{
+    public class CardsContext : DbContext
+    {
+        public CardsContext(DbContextOptions<CardsContext> options) : base(options)
+        {
+        }
+
+        public DbSet<MonsterCard> MonsterCards { get; set; }
+        public DbSet<SpellAndTrapCard> SpellAndTrapCards { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MonsterCard>().HasIndex(card => new { card.Name, card.CardId }).IsUnique();
+            modelBuilder.Entity<SpellAndTrapCard>().HasIndex(card => new { card.Name, card.CardId }).IsUnique();
+        }
+    }
+}
