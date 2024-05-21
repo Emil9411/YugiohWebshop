36b1472d4e5a7bf6c87ee77e067ee4b91d5a66f1
Emil9411 <almadi.emil94@gmail.com>
2024-05-21 21:56:51 +0200

    Model to handle both kind of cards at once created.

---
+++ Yugioh.Server/Model/AllCardResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Yugioh.Server.Model
+{
+    public class AllCardResponse
+    {
+        public List<MonsterCard>? MonsterCards { get; set; } = new List<MonsterCard>();
+        public List<SpellAndTrapCard>? SpellAndTrapCards { get; set; } = new List<SpellAndTrapCard>();
+        public bool Error { get; set; }
+    }
+}
