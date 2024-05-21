e94728f8c8e5f8b03dcc6f4c24b5531b74f2f151
Emil9411 <almadi.emil94@gmail.com>
2024-05-21 22:01:47 +0200

    Interface for handling all-card database operations created.

---
+++ Yugioh.Server/Services/CardRepository/ICardRepoAllCard.cs
@@ -0,0 +1,11 @@
+﻿using Yugioh.Server.Model;
+
+namespace Yugioh.Server.Services.CardRepository
+{
+    public interface ICardRepoAllCard
+    {
+        Task InitialDatabaseFill(AllCardResponse allCardResponse);
+        Task UpdateDatabase(AllCardResponse allCardResponse);
+        Task EmptyDatabase();
+    }
+}
