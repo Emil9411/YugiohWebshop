bd550f309f670ecb65039a2021da11ad44537589
Emil9411 <almadi.emil94@gmail.com>
2024-05-21 22:02:19 +0200

    Interface for handling single-card database operations created.

---
+++ Yugioh.Server/Services/CardRepository/ICardRepoSingleCard.cs
@@ -0,0 +1,11 @@
+﻿using Yugioh.Server.Model;
+
+namespace Yugioh.Server.Services.CardRepository
+{
+    public interface ICardRepoSingleCard
+    {
+        Task<Card?> GetCardByNameAsync(string name);
+        Task<Card?> GetCardByCardIdAsync(int cardId);
+        Task<Card?> AddCardAsync(Card card);
+    }
+}
