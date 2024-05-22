ae0124443b4781e3ef7f8a2dbf33803cab175ec3
Emil9411 <almadi.emil94@gmail.com>
2024-05-22 17:14:01 +0200

    Test object, CardComponent removed, 2 buttons wrapped with <Link>

--- yugioh.client/src/App.jsx
+++ yugioh.client/src/App.jsx
@@ -1,26 +1,7 @@
 import { useEffect, useState } from 'react';
+import { Outlet, Link, useLocation } from "react-router-dom";
 import './index.css';
-import CardComponent from './components/CardComponent';
-
 function App() {
-
-    const testCardObject = {
-        id: 1,
-        cardId: 89631139,
-        name: 'Blue-Eyes White Dragon',
-        type: 'Normal Monster',
-        frameType: 'normal',
-        desc: 'This legendary dragon is a powerful engine of destruction. Virtually invincible, very few have faced this awesome creature and lived to tell the tale.',
-        atk: 3000,
-        def: 2500,
-        level: 8,
-        race: 'Dragon',
-        attribute: 'LIGHT',
-        archetype: 'Blue-Eyes',
-        price: 0.35,
-        ygouProDeckUrl: 'https://ygoprodeck.com/card/blue-eyes-white-dragon-7485'
-    };
-
 
     const updateBackgroundClass = () => {
         const width = window.innerWidth;
@@ -51,7 +32,12 @@ function App() {
                     <h1>Yugioh Webshop</h1>
                 </div>
                 <div className="header-buttons">
-                    <button>All cards</button>
+                    <Link to="/">
+                        <button>Home</button>
+                    </Link>
+                    <Link to="/all">
+                        <button>All cards</button>
+                    </Link>
                     <button>Monster cards</button>
                     <button>Spell/Trap cards</button>
                     <button>Register</button>
@@ -60,7 +46,7 @@ function App() {
                     <button>Cart</button>
                 </div>
             </div>
-            <CardComponent cardData={testCardObject} />
+            <Outlet />
         </div>
 
     );
