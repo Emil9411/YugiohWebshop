d1f3ad01de6cf8c8eac160d2b47df0402fc8843f
Emil9411 <almadi.emil94@gmail.com>
2024-05-22 12:38:04 +0200

    Test card object created to test the CardComponent.

--- yugioh.client/src/App.jsx
+++ yugioh.client/src/App.jsx
@@ -1,7 +1,26 @@
 import { useEffect, useState } from 'react';
 import './index.css';
+import CardComponent from './components/CardComponent';
 
 function App() {
+
+    const testCardObject = {
+        id: 1,
+        cardId: 89631139,
+        name: 'Blue-Eyes White Dragon',
+        type: 'Normal Monster',
+        frameType: 'normal',
+        desc: 'This legendary dragon is a powerful engine of destruction. Virtually invincible, very few have faced this awesome creature and lived to tell the tale.',
+        atk: 3000,
+        def: 2500,
+        level: 8,
+        race: 'Dragon',
+        attribute: 'LIGHT',
+        archetype: 'Blue-Eyes',
+        price: 0.35,
+        ygouProDeckUrl: 'https://ygoprodeck.com/card/blue-eyes-white-dragon-7485'
+    };
+
 
     const updateBackgroundClass = () => {
         const width = window.innerWidth;
@@ -26,19 +45,26 @@ function App() {
     }, []);
 
     return (
-        <div>
-            <h1 id="tabelLabel">Yugioh Webshop</h1>
-            <button>All cards</button>
-            <button>Monster cards</button>
-            <button>Spell/Trap cards</button>
-            <button>Register</button>
-            <button>Login</button>
-            <button>Logout</button>
-            <button>Cart</button>
-            
+        <div className="App">
+            <div className="header">
+                <div className="header-title">
+                    <h1>Yugioh Webshop</h1>
+                </div>
+                <div className="header-buttons">
+                    <button>All cards</button>
+                    <button>Monster cards</button>
+                    <button>Spell/Trap cards</button>
+                    <button>Register</button>
+                    <button>Login</button>
+                    <button>Logout</button>
+                    <button>Cart</button>
+                </div>
+            </div>
+            <CardComponent cardData={testCardObject} />
         </div>
+
     );
-    
+
 }
 
 export default App;
