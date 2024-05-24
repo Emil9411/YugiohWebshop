8dabdedf888aeb0fec4f1f89c3e074ad18eedbaf
Emil9411 <almadi.emil94@gmail.com>
2024-05-24 23:37:09 +0200

    Removed unnecessary import.

--- yugioh.client/src/components/Pagination.jsx
+++ yugioh.client/src/components/Pagination.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 function Pagination({ currentPage, pagesCount, handlePageChange, maxPageButtons }) {
     const renderPageButtons = () => {
         const buttons = [];
