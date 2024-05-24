f9f8265576b405ed0cc8231a50b110eefc1f925e
Emil9411 <almadi.emil94@gmail.com>
2024-05-24 03:06:05 +0200

    New pagination component used.

--- yugioh.client/src/pages/AllCardsPage.jsx
+++ yugioh.client/src/pages/AllCardsPage.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import CardComponent from '../components/CardComponent';
+import Pagination from '../components/Pagination';
 import '../index.css';
 import '../components/Component.css';
 
@@ -46,37 +47,6 @@ function AllCardsPage() {
         }
     };
 
-
-    const renderPageButtons = () => {
-        const buttons = [];
-        const middleIndex = Math.floor(maxPageButtons / 2);
-        let startPage = currentPage - middleIndex;
-        let endPage = currentPage + middleIndex;
-
-        if (startPage < 1) {
-            startPage = 1;
-            endPage = Math.min(startPage + maxPageButtons - 1, pagesCount);
-        } else if (endPage > pagesCount) {
-            endPage = pagesCount;
-            startPage = Math.max(endPage - maxPageButtons + 1, 1);
-        }
-
-        for (let i = startPage; i <= endPage; i++) {
-            buttons.push(
-                <button
-                    key={i}
-                    onClick={() => handlePageChange(i)}
-                    className={i === currentPage ? 'active' : ''}
-                    disabled={i === currentPage}
-                >
-                    {i}
-                </button>
-            );
-        }
-
-        return buttons;
-    };
-
     if (loading) {
         return (
             <>
@@ -90,26 +60,16 @@ function AllCardsPage() {
             <br />
             <h1>All Cards</h1>
             <br />
-            <div className="pagination">
-                <button onClick={() => { setCurrentPage(1); window.scrollTo(0, 0); }} disabled={currentPage === 1}>First</button>
-                <button onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1}>Previous</button>
-                {renderPageButtons()}
-                <button onClick={() => handlePageChange(currentPage + 1)} disabled={currentPage === pagesCount}>Next</button>
-                <button onClick={() => { setCurrentPage(pagesCount); window.scrollTo(0, 0); }} disabled={currentPage === pagesCount}>Last</button>
-            </div><div className="card-container">
+            <Pagination currentPage={currentPage} pagesCount={pagesCount} handlePageChange={handlePageChange} />
+            <div className="card-container">
                 {currentCards.map((card, i) => (
                     <div key={i} className="card-wrapper">
                         <CardComponent cardData={card} />
                     </div>
                 ))}
-                <div className="pagination">
-                    <button onClick={() => { setCurrentPage(1); window.scrollTo(0, 0); }} disabled={currentPage === 1}>First</button>
-                    <button onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1}>Previous</button>
-                    {renderPageButtons()}
-                    <button onClick={() => handlePageChange(currentPage + 1)} disabled={currentPage === pagesCount}>Next</button>
-                    <button onClick={() => { setCurrentPage(pagesCount); window.scrollTo(0, 0); }} disabled={currentPage === pagesCount}>Last</button>
-                </div>
             </div>
+            <br />
+            <Pagination currentPage={currentPage} pagesCount={pagesCount} handlePageChange={handlePageChange} />
         </>
     );
 }
