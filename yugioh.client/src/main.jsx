04b018323939b73c56b1a9cfa85c4431026d9c52
Emil9411 <almadi.emil94@gmail.com>
2024-05-25 15:02:13 +0200

    New page component got a path to reach it.

--- yugioh.client/src/main.jsx
+++ yugioh.client/src/main.jsx
@@ -4,6 +4,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from './App.jsx'
 import './index.css'
 import AllCardsPage from './pages/AllCardsPage.jsx'
+import MonsterCardsPage from './pages/MonsterCardsPage.jsx'
 
 const router = createBrowserRouter([
     {
@@ -14,6 +15,10 @@ const router = createBrowserRouter([
                 path: "/all",
                 element: <AllCardsPage />,
             },
+            {
+                path: "/monsters",
+                element: <MonsterCardsPage />,
+            }
         ],
     },
 ]
