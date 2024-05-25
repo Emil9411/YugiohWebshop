6be693e1d73397ebddc61a05e4b078b7760558ac
Emil9411 <almadi.emil94@gmail.com>
2024-05-25 15:01:00 +0200

    The SelectFilter component reworked, so it always shows the correct option after click.

--- yugioh.client/src/components/SelectFilter.jsx
+++ yugioh.client/src/components/SelectFilter.jsx
@@ -1,6 +1,6 @@
-function SelectFilter({ optionsList, handleChange }) {
+function SelectFilter({ optionsList, handleChange, selectedValue }) {
     return (
-        <select onChange={handleChange}>
+        <select value={selectedValue} onChange={handleChange}>
             {optionsList.map((option, index) => {
                 return <option key={index} value={option}>{option}</option>
             })}
