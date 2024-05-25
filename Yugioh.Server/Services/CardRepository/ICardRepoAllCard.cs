3b6b811a6dbd7f6cf65d70eae115e9a67b44a371
Emil9411 <almadi.emil94@gmail.com>
2024-05-25 23:22:25 +0200

    New methods for returning only monster or spell cards created.

--- Yugioh.Server/Services/CardRepository/ICardRepoAllCard.cs
+++ Yugioh.Server/Services/CardRepository/ICardRepoAllCard.cs
@@ -8,5 +8,7 @@ namespace Yugioh.Server.Services.CardRepository
         Task UpdateDatabase(AllCardResponse allCardResponse);
         Task EmptyDatabase();
         Task<AllCardResponse> GetAllCard();
+        Task<List<MonsterCard>> GetAllMonsterCards();
+        Task<List<SpellAndTrapCard>> GetAllSpellCards();
     }
 }
