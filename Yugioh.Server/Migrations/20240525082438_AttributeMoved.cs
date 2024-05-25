5d7dd2837576e0febd7c4c349ff64d82f60d973b
Emil9411 <almadi.emil94@gmail.com>
2024-05-25 11:31:29 +0200

    Database migrated to represent the correct data

---
+++ Yugioh.Server/Migrations/20240525082438_AttributeMoved.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Yugioh.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AttributeMoved : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Attribute",
+                table: "SpellAndTrapCards",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Attribute",
+                table: "SpellAndTrapCards");
+        }
+    }
+}
