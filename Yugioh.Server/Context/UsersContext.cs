8d799705a7f9ee79139f0abcf5b501e482536ea2
Emil9411 <almadi.emil94@gmail.com>
2024-05-27 15:27:36 +0200

    The database context for users created.

---
+++ Yugioh.Server/Context/UsersContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Yugioh.Server.Model;
+
+namespace Yugioh.Server.Context
+{
+    public class UsersContext : IdentityDbContext<User, IdentityRole, string>
+    {
+        public UsersContext(DbContextOptions<UsersContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
