91422663cc769f1ebd45860cfe98de448fb38eed
Emil9411 <almadi.emil94@gmail.com>
2024-05-27 15:29:58 +0200

    AuthResult record created.

---
+++ Yugioh.Server/Services/AuthServices/Models/AuthResult.cs
@@ -0,0 +1,8 @@
+﻿namespace Yugioh.Server.Services.AuthServices.Models
+{
+    public record AuthResult(bool Success, string Email, string Username, string Token)
+    {
+        // Error code - Error message
+        public readonly Dictionary<string, string> ErrorMessages = new();
+    }
+}
