f25449b91710fca5c96b18b9ba0e648a42e5af31
Emil9411 <almadi.emil94@gmail.com>
2024-05-28 11:03:30 +0200

    Interface for authorization services created.

---
+++ Yugioh.Server/Services/AuthServices/Services/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using Yugioh.Server.Services.AuthServices.Models;
+
+namespace Yugioh.Server.Services.AuthServices.Services
+{
+    public interface IAuthService
+    {
+        Task<AuthResult> RegisterAsync(string emial, string username, string password, string role);
+        Task<AuthResult> LoginAsync(string email, string password);
+        JwtSecurityToken Verify(string token);
+    }
+}
