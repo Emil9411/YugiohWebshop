d53d170585ea6185d7dbbddb0f70e8ab219ce8d2
Emil9411 <almadi.emil94@gmail.com>
2024-05-28 20:36:30 +0200

    New methods created for changing password, and changing email.

--- Yugioh.Server/Services/AuthServices/Services/IAuthService.cs
+++ Yugioh.Server/Services/AuthServices/Services/IAuthService.cs
@@ -7,6 +7,8 @@ namespace Yugioh.Server.Services.AuthServices.Services
     {
         Task<AuthResult> RegisterAsync(string emial, string username, string password, string role);
         Task<AuthResult> LoginAsync(string email, string password);
+        Task<AuthResult> ChangePasswordAsync(string email, string currentPassword, string newPassword);
+        Task<AuthResult> ChangeEmailAsync(string email, string newEmail);
         JwtSecurityToken Verify(string token);
     }
 }
