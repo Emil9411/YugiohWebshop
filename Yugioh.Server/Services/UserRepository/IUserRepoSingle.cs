7c3948ad7c2523e134fcf76709b730dbb5a47e7c
Emil9411 <almadi.emil94@gmail.com>
2024-05-28 17:28:39 +0200

    Methods for basic CRUD operations created.

--- Yugioh.Server/Services/UserRepository/IUserRepoSingle.cs
+++ Yugioh.Server/Services/UserRepository/IUserRepoSingle.cs
@@ -1,6 +1,14 @@
-﻿namespace Yugioh.Server.Services.UserRepository
+﻿using Microsoft.AspNetCore.Mvc;
+using Yugioh.Server.Model.UserModels;
+
+namespace Yugioh.Server.Services.UserRepository
 {
     public interface IUserRepoSingle
     {
+        ActionResult<User> GetUserByEmail(string email);
+        ActionResult<User> GetUserById(string id);
+        ActionResult<User> AddAdminUser(User user);
+        ActionResult<User> UpdateUser(User user);
+        ActionResult<User> DeleteUser(User user);
     }
 }
