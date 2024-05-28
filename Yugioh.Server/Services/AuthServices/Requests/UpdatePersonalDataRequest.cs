11c3c59be6159cbc9ab8868f431cd19ecb553143
Emil9411 <almadi.emil94@gmail.com>
2024-05-28 21:12:39 +0200

    Request for updating personal data created.

---
+++ Yugioh.Server/Services/AuthServices/Requests/UpdatePersonalDataRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Yugioh.Server.Services.AuthServices.Requests
+{
+    public record UpdatePersonalDataRequest([Required] string Email,
+                                            [Required] string FirstName,
+                                            [Required] string LastName,
+                                            [Required] string Country,
+                                            [Required] string City,
+                                            [Required] string Address,
+                                            [Required] string PostalCode,
+                                            [Required] string PhoneNumber);
+}
