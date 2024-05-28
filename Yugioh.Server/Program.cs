af809bf77919a06cbf42b684d996375d0a6e5271
Emil9411 <almadi.emil94@gmail.com>
2024-05-28 13:02:36 +0200

    Roles and authentication added, Swagger configured, new services registered.

--- Yugioh.Server/Program.cs
+++ Yugioh.Server/Program.cs
@@ -1,8 +1,17 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.Net.Http.Headers;
+using Microsoft.OpenApi.Models;
+using System.Text;
 using Yugioh.Server.Context;
+using Yugioh.Server.Model;
 using Yugioh.Server.Services.Api;
+using Yugioh.Server.Services.AuthServices.Models;
+using Yugioh.Server.Services.AuthServices.Seeder;
+using Yugioh.Server.Services.AuthServices.Services;
 using Yugioh.Server.Services.BusinessLogic;
 using Yugioh.Server.Services.CardRepository;
 using Yugioh.Server.Services.JsonProcess;
@@ -13,8 +22,16 @@ var builder = WebApplication.CreateBuilder(args);
 AddServices();
 ConfigureSwagger();
 AddDbContext();
+AddIdentity();
+AddAuthentication();
 
 var app = builder.Build();
+
+using var scope = app.Services.CreateScope();
+var authSeeder = scope.ServiceProvider.GetRequiredService<AuthSeeder>();
+authSeeder.AddRoles();
+authSeeder.AddAmin();
+authSeeder.AddTestUser();
 
 app.UseCors();
 
@@ -34,7 +51,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-// app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
@@ -47,13 +64,16 @@ app.Run();
 void AddServices()
 {
     builder.Services.AddControllers();
+    builder.Services.AddLogging();
     builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
     builder.Services.AddScoped<IBusinessAllCard, Business>();
     builder.Services.AddScoped<IBusinessSingleCard, Business>();
     builder.Services.AddScoped<ICardRepoAllCard, CardRepo>();
     builder.Services.AddScoped<ICardRepoSingleCard, CardRepo>();
     builder.Services.AddScoped<IRandomRowSelector, RandomRowSelector>();
+    builder.Services.AddScoped<ITokenService, TokenService>();
+    builder.Services.AddScoped<IAuthService, AuthService>();
+    builder.Services.AddScoped<AuthSeeder>();
     builder.Services.AddSingleton<IYugiohApiAllCard, YugiohApi>();
     builder.Services.AddSingleton<IYugiohApiSingleCard, YugiohApi>();
     builder.Services.AddSingleton<IJsonProcessAllCard, YugiohJsonProcessor>();
@@ -72,7 +92,33 @@ void AddServices()
 
 void ConfigureSwagger()
 {
-    builder.Services.AddSwaggerGen();
+    builder.Services.AddSwaggerGen(option =>
+    {
+        option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+        option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            In = ParameterLocation.Header,
+            Description = "Please enter a valid token",
+            Name = "Authorization",
+            Type = SecuritySchemeType.Http,
+            BearerFormat = "JWT",
+            Scheme = "Bearer"
+        });
+        option.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                Array.Empty<string>()
+            }
+        });
+    });
 }
 void AddDbContext()
 {
@@ -85,4 +131,58 @@ void AddDbContext()
     {
         options.UseSqlServer(connectionString);
     });
+}
+
+void AddIdentity()
+{
+    builder.Services
+        .AddIdentityCore<User>(options =>
+        {
+            options.SignIn.RequireConfirmedAccount = false;
+            options.User.RequireUniqueEmail = true;
+            options.Password.RequireDigit = false;
+            options.Password.RequiredLength = 6;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequireLowercase = false;
+        })
+        .AddRoles<IdentityRole>()
+        .AddEntityFrameworkStores<UsersContext>();
+}
+
+void AddAuthentication()
+{
+    builder.Services
+        .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddCookie(options =>
+        {
+            options.Cookie.Name = "Authorization";
+        })
+        .AddJwtBearer(options =>
+        {
+            var jwtSettings = builder.Configuration.GetSection("JwtSettings").Get<JwtSettings>() ?? new JwtSettings();
+            var issuerSigningKey = builder.Configuration.GetSection("IssuerSigningKey").Value ?? "";
+            options.TokenValidationParameters = new TokenValidationParameters()
+            {
+                ClockSkew = TimeSpan.Zero,
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = jwtSettings.ValidIssuer,
+                ValidAudience = jwtSettings.ValidAudience,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(issuerSigningKey)),
+            };
+            options.Events = new JwtBearerEvents
+            {
+                OnMessageReceived = context =>
+                {
+                    if (context.Request.Cookies.ContainsKey("Authorization"))
+                    {
+                        context.Token = context.Request.Cookies["Authorization"];
+                    }
+                    return Task.CompletedTask;
+                }
+            };
+        });
 }
