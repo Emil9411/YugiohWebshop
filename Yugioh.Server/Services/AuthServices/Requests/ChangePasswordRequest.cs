7ac349870a7e336932f440f9ce0c9a341958bbe0
Emil9411 <almadi.emil94@gmail.com>
2024-05-28 20:25:00 +0200

    Model for requesting password change created.

---
+++ Yugioh.Server/Services/AuthServices/Requests/ChangePasswordRequest.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Yugioh.Server.Services.AuthServices.Requests
+{
+    public record ChangePasswordRequest([Required] string Email,
+                                      [Required] string OldPassword,
+                                      [Required] string NewPassword);
+}
