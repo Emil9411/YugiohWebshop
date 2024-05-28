167e76f06d8749b338157d82e9877a557a0bc559
Emil9411 <almadi.emil94@gmail.com>
2024-05-28 20:25:25 +0200

    Model for requesting email address update created.

---
+++ Yugioh.Server/Services/AuthServices/Requests/UpdateEmailRequest.cs
@@ -0,0 +1,7 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Yugioh.Server.Services.AuthServices.Requests
+{
+    public record UpdateEmailRequest([Required] string Email,
+                                     [Required] string NewEmail);
+}
