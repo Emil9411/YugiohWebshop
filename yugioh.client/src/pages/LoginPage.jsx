a3578da0d003780925e939982cfa891064e961dd
Emil9411 <almadi.emil94@gmail.com>
2024-05-29 20:24:01 +0200

    Base of the login page created.

---
+++ yugioh.client/src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import "../index.css";
+
+function LoginPage() {
+  return (
+    <p>Hello world!</p>
+  );
+}
+
+export default LoginPage;
