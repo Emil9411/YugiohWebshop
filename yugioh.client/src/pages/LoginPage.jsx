bb0041c75a2f2303e665884104d473a5499aebfc
Emil9411 <almadi.emil94@gmail.com>
2024-05-29 20:58:41 +0200

    Login page is in working condition.

--- yugioh.client/src/pages/LoginPage.jsx
+++ yugioh.client/src/pages/LoginPage.jsx
@@ -3,9 +3,84 @@ import { useNavigate } from 'react-router-dom';
 import "../index.css";
 
 function LoginPage() {
-  return (
-    <p>Hello world!</p>
-  );
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [formSubmitted, setFormSubmitted] = useState(false);
+    const navigate = useNavigate();
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        setFormSubmitted(true);
+        if (!email || !password) {
+            return;
+        }
+        try {
+            const response = await fetch("api/Auth/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    email,
+                    password
+                }),
+            });
+
+            if (response.ok) {
+                navigate("/");
+            } else {
+                throw new Error("Login failed");
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    return (
+        <div className="login-page">
+            <h2>Login</h2>
+            <form onSubmit={handleSubmit}>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>
+                                {formSubmitted && email === "" ? <p style={{ color: "red" }}>Email is required</p> : <p>Email:</p>}
+                            </td>
+                            <td>
+                                <input
+                                    type="text"
+                                    value={email}
+                                    onChange={(event) => setEmail(event.target.value)}
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                {formSubmitted && password === "" ? <p style={{ color: "red" }}>Password is required</p> : <p>Password:</p>}
+                            </td>
+                            <td>
+                                <input
+                                    type="password"
+                                    value={password}
+                                    onChange={(event) => setPassword(event.target.value)}
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <button onClick={() => navigate("/")} type="button">Cancel</button>
+                            </td>
+                            <td>
+                                <button type="submit">Login</button>
+                            </td>
+                        </tr>
+                        <tr rowSpan="2">
+                            <td colSpan="2"></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </form>
+        </div>
+    );
 }
 
 export default LoginPage;
