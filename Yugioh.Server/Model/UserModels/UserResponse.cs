b674aab92064e5b1964d678f8a58f5b083206738
Emil9411 <almadi.emil94@gmail.com>
2024-05-31 15:31:10 +0200

    UserResponse model now returns the username too.

--- Yugioh.Server/Model/UserModels/UserResponse.cs
+++ Yugioh.Server/Model/UserModels/UserResponse.cs
@@ -2,6 +2,7 @@
 {
     public class UserResponse
     {
+        public string? UserName { get; set; }
         public string? Email { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
