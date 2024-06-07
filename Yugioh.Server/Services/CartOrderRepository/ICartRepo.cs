314b572dc7231b00fe9f3a9c2ffd57465bf848a3
Emil9411 <almadi.emil94@gmail.com>
2024-06-07 18:37:04 +0200

    Interfaces for the cart and order created.

---
+++ Yugioh.Server/Services/CartOrderRepository/ICartRepo.cs
@@ -0,0 +1,24 @@
+﻿using Yugioh.Server.Model.CartModels;
+
+namespace Yugioh.Server.Services.CartOrderRepository
+{
+    public interface ICartRepo
+    {
+        // Creating a new cart for a user
+        Task<Cart> CreateCart(string userId);
+        // Getting a cart by user id
+        Task<Cart> GetCart(string userId);
+        // Adding a card to the cart
+        Task AddCardToCart(string userId, int cardId, int quantity);
+        // Removing a card from the cart
+        Task RemoveCardFromCart(string userId, int cardId);
+        // Updating the quantity of a card in the cart
+        Task UpdateCardQuantity(string userId, int cardId, int quantity);
+        // Getting all the items in the cart
+        Task<List<CartItem>> GetCartItems(string userId);
+        // Clearing the cart
+        Task ClearCart(string userId);
+        // Checking out the cart
+        Task<Order> CheckoutCart(string userId, Order order);
+    }
+}
---
+++ Yugioh.Server/Services/CartOrderRepository/IOrderRepo.cs
@@ -0,0 +1,20 @@
+﻿using Yugioh.Server.Model.CartModels;
+
+namespace Yugioh.Server.Services.CartOrderRepository
+{
+    public interface IOrderRepo
+    {
+        // Creating a new order for a user after checking out
+        Task<Order> CreateOrder(string userId, Order order);
+        // Getting all the orders of a user
+        Task<List<Order>> GetOrders(string userId);
+        // Getting an order by order id
+        Task<Order> GetOrder(int orderId);
+        // Updating the order
+        Task<Order> UpdateOrder(Order order);
+        // Deleting the order
+        Task DeleteOrder(int orderId);
+        // Getting all the orders in the system (admin only)
+        Task<List<Order>> GetAllOrders();
+    }
+}
