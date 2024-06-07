d44afe4bd3294c1dbb71bd772095e461b61b52a8
Emil9411 <almadi.emil94@gmail.com>
2024-06-07 17:30:46 +0200

    New Cart model created to track the cards put into the cart.

---
+++ Yugioh.Server/Model/CartModels/Cart.cs
@@ -0,0 +1,60 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Yugioh.Server.Model.CartModels
+{
+    public class Cart
+    {
+        [Key]
+        public int CartId { get; set; }
+        public string? UserId { get; set; }
+        public bool IsCheckedOut { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+        public virtual ICollection<CartItem> CartItems { get; set; } = new List<CartItem>();
+        public virtual Order? Order { get; set; }
+
+        public void UpdateTimestamp()
+        {
+            UpdatedAt = DateTime.Now;
+        }
+
+        public void AddItem(CartItem item)
+        {
+            CartItems.Add(item);
+            UpdateTimestamp();
+        }
+        public void RemoveItem(CartItem item)
+        {
+            CartItems.Remove(item);
+            UpdateTimestamp();
+        }
+        public void ClearItems()
+        {
+            CartItems.Clear();
+            UpdateTimestamp();
+        }
+
+        public double GetTotalPrice()
+        {
+            if (CartItems.Count == 0 || CartItems == null)
+            {
+                return 0;
+            }
+            double totalPrice = 0;
+            foreach (var item in CartItems)
+            {
+                totalPrice += ConvertStringToDouble(item.Type == "Monster" ? item.MonsterProduct?.Price : item.SpellAndTrapProduct?.Price) * item.Quantity;
+            }
+            return totalPrice;
+        }
+
+        private double ConvertStringToDouble(string value)
+        {
+            if (value == null || value == "")
+            {
+                return 0;
+            }
+            return double.Parse(value, System.Globalization.CultureInfo.InvariantCulture);
+        }
+    }
+}
